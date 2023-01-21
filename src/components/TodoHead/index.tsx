ee675bd2f3d8b5bbbc4b79ac0eb84893f7665b61
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-01-22 00:44:35 +0900

    feat: update TodoHead to display current date

--- src/components/TodoHead/index.tsx
+++ src/components/TodoHead/index.tsx
@@ -1,11 +1,28 @@
 import * as Styled from "./index.styled";
+import { useTodoState } from "@/src/store/TodoContext";
 
 const TodoHead = () => {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+
+  // * Date.toLocaleDateString
+  // Converts a date to a string by using the current or specified locale.
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", {
+    weekday: "long",
+  });
+
   return (
     <Styled.TodoHeadBlock>
-      <h1>2023년 1월 19일</h1>
-      <p className="day">일요일</p>
-      <p className="tasks-left">할 일 2개 남음</p>
+      <h1>{dateString}</h1>
+      <p className="day">{dayName}</p>
+      <p className="tasks-left">할 일 {undoneTasks.length}개 남음</p>
     </Styled.TodoHeadBlock>
   );
 };
